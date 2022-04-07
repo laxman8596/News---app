3e2f0176a49754b1c32b1a377d9b4bbc23cb0b01
laxman8596 <annaboinalaxman6@gmail.com>
2022-04-07 20:25:42 +0530

    first commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -88,31 +88,30 @@ const Home = () => {
 			<div className='row' style={{ backgroundColor: '#F0F8FF' }}>
 
 				{
-					data !== [] ?
-						data.map(news =>
-							<div className='col-md-4' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-								<div className='card' style={{ width: '350px', margin: '10px', borderRadius: '20px' }}>
-									<img className='card-img-top' src={news.urlToImage} height="250" width="260"
-										style={{ borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }} alt="img" />
-									<div className='card-body text-center' >
-										<h6>{news.title}</h6>
-										<p>{news.description}</p>
-										<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-											<p style={{ fontSize: '10px' }}> Author : <strong>{news.author}</strong></p>
-											<p style={{ fontSize: '10px' }}> <strong>Date : </strong>{news.publishedAt}</p>
 
-										</div>
-
-										< a href={news.url} target="_blank" rel="noopener noreferrer " > <button type="button" class="btn btn-primary btn-sm" >Read More</button></a >
-
+					data.map(news =>
+						<div className='col-md-4' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+							<div className='card' style={{ width: '350px', margin: '10px', borderRadius: '20px' }}>
+								<img className='card-img-top' src={news.urlToImage} height="250" width="260"
+									style={{ borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }} alt="img" />
+								<div className='card-body text-center' >
+									<h6>{news.title}</h6>
+									<p>{news.description}</p>
+									<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+										<p style={{ fontSize: '10px' }}> Author : <strong>{news.author}</strong></p>
+										<p style={{ fontSize: '10px' }}> <strong>Date : </strong>{news.publishedAt}</p>
 
 									</div>
 
+									< a href={news.url} target="_blank" rel="noopener noreferrer " > <button type="button" class="btn btn-primary btn-sm" >Read More</button></a >
+
+
 								</div>
+
 							</div>
-						)
-						:
-						null
+						</div>
+					)
+
 				}
 			</div>
 		</>
